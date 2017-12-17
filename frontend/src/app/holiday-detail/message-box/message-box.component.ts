47ee94fd79ad91267a461f1472dbc55676bcc774
bstein <bstein@thoughtworks.com>
2017-12-17 23:10:02 +0100

    add date and name to messages

--- frontend/src/app/holiday-detail/message-box/message-box.component.ts
+++ frontend/src/app/holiday-detail/message-box/message-box.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
+import {Moment} from 'moment';
+
+
+export class Message {
+  constructor(readonly author: string, readonly message: string, readonly created: Moment) {}
+}
 
 @Component({
   selector: 'app-message-box',
@@ -7,9 +14,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessageBoxComponent implements OnInit {
 
+  messages: Message[] = [];
+
   constructor() { }
 
   ngOnInit() {
+    this.messages.push(new Message('Benni', 'hallo ihr omas! ist der glöckner auch am start?', moment('2016-01-01')));
+    this.messages.push(new Message('Sarah', 'selber oma hihi', moment('2016-01-02')));
+    this.messages.push(new Message('Jens', 'ja ich bin am start!', moment('2016-01-03')));
   }
 
 }
