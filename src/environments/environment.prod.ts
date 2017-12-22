f233a85ad5be824968701f6f1335c3d1479047ae
bstein <bstein@thoughtworks.com>
2017-12-22 23:47:45 +0100

    satisfy linter

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyB2GSJvXjGcJVjHR5HQvIvwdsS0Y12wAkE",
-    authDomain: "mboyz-1e2c2.firebaseapp.com",
-    databaseURL: "https://mboyz-1e2c2.firebaseio.com",
-    projectId: "mboyz-1e2c2",
-    storageBucket: "mboyz-1e2c2.appspot.com",
-    messagingSenderId: "563676792074"
+    apiKey: 'AIzaSyB2GSJvXjGcJVjHR5HQvIvwdsS0Y12wAkE',
+    authDomain: 'mboyz-1e2c2.firebaseapp.com',
+    databaseURL: 'https://mboyz-1e2c2.firebaseio.com',
+    projectId: 'mboyz-1e2c2',
+    storageBucket: 'mboyz-1e2c2.appspot.com',
+    messagingSenderId: '563676792074'
   }
 };
