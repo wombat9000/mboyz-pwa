8e6b737de64431ed28f43cce1abb7782a7a6a29d
bstein <bstein@thoughtworks.com>
2018-04-27 11:02:00 +0200

    inline login page template

--- src/app/auth/services/user-firestore.service.ts
+++ src/app/auth/services/user-firestore.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 import {AngularFirestore} from 'angularfire2/firestore';
 import {MtravelUser} from './auth.service';
 import {Observable} from 'rxjs/Observable';
-import {Action} from '@ngrx/store';
+
 
 @Injectable()
 export class UserFirestore {
