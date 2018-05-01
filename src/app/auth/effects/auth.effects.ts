5054b711457f31f57f6dc4626f5766c06a168551
bstein <bstein@thoughtworks.com>
2018-05-01 15:49:29 +0200

    use pipes in auth effects, remove log statements

--- src/app/auth/effects/auth.effects.ts
+++ src/app/auth/effects/auth.effects.ts
@@ -1,38 +1,31 @@
 import {Injectable} from '@angular/core';
 import {Router} from '@angular/router';
 import {AuthService} from '../services/auth.service';
-import {Actions, Effect} from '@ngrx/effects';
+import {Actions, Effect, ofType} from '@ngrx/effects';
 import {Observable} from 'rxjs/Observable';
-import {
-  AuthActionTypes,
-  Authorise,
-  LoginFailure,
-  LoginSuccess,
-  NotAuthenticated,
-  Unauthorised
-} from '../actions/auth.actions';
+import {AuthActionTypes, Authorise, LoginFailure, LoginSuccess, NotAuthenticated, Unauthorised} from '../actions/auth.actions';
 import {Action} from '@ngrx/store';
 import {AngularFirestore} from 'angularfire2/firestore';
 import {AngularFireAuth} from 'angularfire2/auth';
 import {UserFirestore} from '../services/user-firestore.service';
+import {map, switchMap} from 'rxjs/operators';
 
 
 @Injectable()
 export class AuthEffects {
 
   @Effect({dispatch: false})
-  authorise$: Observable<any> = this.actions$
-    .ofType(AuthActionTypes.AUTHORISE)
-    .switchMap((action: Authorise) => {
-      console.log(`[AuthFX] Authorised, redirecting to ${action.payload.url}`);
+  authorise$: Observable<any> = this.actions$.pipe(
+    ofType(AuthActionTypes.AUTHORISE),
+    switchMap((action: Authorise) => {
       return Observable.fromPromise(this.router.navigate([action.payload.url]));
-    });
+    })
+  );
 
   @Effect()
-  unauthorised$: Observable<Action> = this.actions$
-    .ofType(AuthActionTypes.UNAUTHORISED)
-    .do(() => console.log('[AuthFX] Unauthorised, attempting to authorise...'))
-    .switchMap((action: Unauthorised) => {
+  unauthorised$: Observable<Action> = this.actions$.pipe(
+    ofType(AuthActionTypes.UNAUTHORISED),
+    switchMap((action: Unauthorised) => {
       return this.afsAuth.authState.map(afUser => {
         if (afUser) {
           const user = {
@@ -45,44 +38,44 @@ export class AuthEffects {
         }
         return new NotAuthenticated();
       });
-    });
+    })
+  );
 
   @Effect()
-  fbLogin$: Observable<Action> = this.actions$
-    .ofType(AuthActionTypes.FACEBOOK_LOGIN)
-    .switchMap(() => {
+  fbLogin$: Observable<Action> = this.actions$.pipe(
+    ofType(AuthActionTypes.FACEBOOK_LOGIN),
+    switchMap(() => {
       return this.authService.facebookLogin()
         .switchMap(user => this.userFirestore.save(user).map(() => user))
         .map(user => new LoginSuccess({user: user}))
         .catch(err => {
-          console.error(`[AuthFX] Login failed: ${err}`);
           return Observable.of(new LoginFailure({error: err.message}));
         });
-    });
+    })
+  );
 
   @Effect({dispatch: false})
-  loginSuccess$: Observable<boolean> = this.actions$
-    .ofType(AuthActionTypes.LOGIN_SUCCESS)
-    .do(() => console.log('[AuthFX] Login successful, redirecting to home'))
-    .switchMap(() => {
+  loginSuccess$: Observable<boolean> = this.actions$.pipe(
+    ofType(AuthActionTypes.LOGIN_SUCCESS),
+    switchMap(() => {
       return Observable.fromPromise(this.router.navigate(['/']));
-    });
+    })
+  );
 
   @Effect({dispatch: false})
-  notAuthenticated$: Observable<boolean> = this.actions$
-    .ofType(AuthActionTypes.NOT_AUTHENTICATED)
-    .do(() => console.log('[AuthFX] Not authenticated, redirecting to /login'))
-    .switchMap(() => {
+  notAuthenticated$: Observable<boolean> = this.actions$.pipe(
+    ofType(AuthActionTypes.NOT_AUTHENTICATED),
+    switchMap(() => {
       return Observable.fromPromise(this.router.navigate(['/login']));
-    });
+    })
+  );
 
   @Effect()
-  logout$: Observable<Action> = this.actions$
-    .ofType(AuthActionTypes.LOGOUT)
-    .do(() => console.log('[AuthFX] Login successful, redirecting to home'))
-    .map(() => Observable.fromPromise(this.afsAuth.auth.signOut()))
-    .map(() => new NotAuthenticated());
-
+  logout$: Observable<Action> = this.actions$.pipe(
+    ofType(AuthActionTypes.LOGOUT),
+    map(() => Observable.fromPromise(this.afsAuth.auth.signOut())),
+    map(() => new NotAuthenticated())
+  );
 
   constructor(private actions$: Actions,
               private afs: AngularFirestore,
