3ef6244202d493883680f127e08cd8a7c6074030
bstein <bstein@thoughtworks.com>
2018-06-20 22:50:47 +0200

    remove some protractor leftovers

--- jest.config.js
+++ jest.config.js
@@ -3,27 +3,3 @@ module.exports = {
   roots: ['src'],
   setupTestFrameworkScriptFile: '<rootDir>/src/setup-jest.ts'
 };
-
-//
-// "jest": {
-//   "moduleFileExtensions": [
-//     "ts",
-//     "html",
-//     "js",
-//     "json"
-//   ],
-//     "transform": {
-//     "^.+\\.(tsx?|html)$": "ts-jest",
-//       "^.+\\.(ts|tsx)$": "ts-jest"
-//   },
-//   "globals": {
-//     "__TRANSFORM_HTML__": true,
-//       "ts-jest": {
-//       "tsConfigFile": "tsconfig.json"
-//     }
-//   },
-//   "testMatch": [
-//     "<rootDir>/src/**/*.+(spec.ts)"
-//   ],
-//     "setupTestFrameworkScriptFile": "<rootDir>/src/setupJestTestFramework.ts"
-// }
